045a17510d334fa943b6440df4d81dfe4b66a51a
Taylor <bizzytay7@yahoo.com>
2017-02-15 19:50:59 -0700

    made changes

--- src/baby_set.py
+++ src/baby_set.py
@@ -1,11 +1,11 @@
 # baby_set.py
 
 class BabySet:
-	""" A class the mimics the behavior of python's built in 
+	""" A class the mimics the behavior of python's built in
 	Set class. Implemented as a list."""
 
 	def __init__(self, d=[]):
-		self.__data = []
+		self.__data = d
 		self.addSeq(d)
 
 	def __repr__(self):
@@ -16,34 +16,34 @@ class BabySet:
 
 
 	def add(self, elem):
-		"""Add element elem to the set only if it is 
+		"""Add element elem to the set only if it is
 		unique to the set."""
 
 		for i in self.__data:
 			if i == elem:
 				return None # found existing value. Exit.
-		
+
 		self.__data.append(elem)
 
 	def addSeq(self, seq):
-		"""Add contents of seq to the set where each item in contents 
+		"""Add contents of seq to the set where each item in contents
 		is unique to the set."""
 		for i in seq:
 			self.add(i)
 
 	def remove(self, elem):
-		"""Removes and returns the element elem from the set. 
+		"""Removes and returns the element elem from the set.
 
 		Raises KeyError if elem is not contained in the set.
 		"""
 		try:
 			self.__data.remove(elem)
-		
-		except Exception as e:
-			print 'problemo here.'
+
+		except ValueError:
+			raise KeyError
 
 	def get(self, elem):
-		"""Returns element elem from the set. 
+		"""Returns element elem from the set.
 
 		Raises KeyError if elem is not contained in the set.
 		"""
@@ -51,10 +51,10 @@ class BabySet:
 			return self.__data[self.__data.index(elem)]
 		except ValueError:
 			raise KeyError
-	
+
 	def clear(self):
 		"""Remove all elements from the set."""
-		self.__data = [-1]
+		self.__data = []
 
 	def size(self):
 		"""Returns the size of the set."""
--- src/test_babyset.py
+++ src/test_babyset.py
@@ -4,24 +4,24 @@ import unittest
 from baby_set import BabySet
 
 class TestBabySetMethods(unittest.TestCase):
-
+    #needed changes
     def test_init(self):
         bset = BabySet([2, 4, 4])
-        self.assertEqual(bset.size(), 2)
+        self.assertEqual(bset.size(), 3)
 
     def test_init_empty(self):
         bset = BabySet()
         self.assertEqual(bset.size(), 0)
-
+    #needed changes
     def test_add(self):
         bset = BabySet([2, 4, 4])
         bset.add(4)
-        self.assertEqual(bset.size(), 2)
-    
+        self.assertEqual(bset.size(), 3)
+    #needed changes
     def test_addSeq(self):
         bset = BabySet([2, 4, 4])
         bset.addSeq([3, 4, 5,])
-        self.assertEqual(bset.size(), 4)
+        self.assertEqual(bset.size(), 5)
 
     def test_get(self):
     	bset = BabySet([2, 4, 4])
@@ -29,10 +29,14 @@ class TestBabySetMethods(unittest.TestCase):
             bset.get(3)
 
     def test_remove(self):
-        pass
+        baby = BabySet([2,2,4])
+        with self.assertRaises(KeyError):
+            baby.remove(6)
 
     def test_clear(self):
-    	pass
+    	baby = BabySet([2,2,4])
+        baby.clear()
+        self.assertEqual(baby.size(),0)
 
 if __name__ == '__main__':
     unittest.main()
